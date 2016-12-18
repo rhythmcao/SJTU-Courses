8ba3725b68a84e4bd95685d4fb36ba00a1e3d937
rhythmCao <ruishengcao@gmail.com>
2016-12-18 16:39:03 +0800

    a try

--- src/simpl/interpreter/Interpreter.java
+++ src/simpl/interpreter/Interpreter.java
@@ -16,8 +16,10 @@ public class Interpreter {
             Parser parser = new Parser(inp);
             java_cup.runtime.Symbol parseTree = parser.parse();
             Expr program = (Expr) parseTree.value;
+
             System.out.println(program.typecheck(new DefaultTypeEnv()).t);
             System.out.println(program.eval(new InitialState()));
+
         }
         catch (SyntaxError e) {
             System.out.println("syntax error");
@@ -40,29 +42,29 @@ public class Interpreter {
     }
 
     public static void main(String[] args) {
-      String file = args[0];
-      if (args.length == 0)
-          throw new RuntimeException("no file");
-      if (!args[0].matches(".+\\.spl"))
-          throw new RuntimeException("Error filename extension");
-      interpret(file);
-      System.out.println("Execute complete!");
+//      String file = args[0];
+//      if (args.length == 0)
+//          throw new RuntimeException("no file");
+//      if (!args[0].matches(".+\\.spl"))
+//          throw new RuntimeException("Error filename extension");
+//      interpret(file);
+//      System.out.println("Execute complete!");
 //        //Good cases
-//        interpret("doc/examples/plus.spl");
-//        interpret("doc/examples/max.spl");
-//        interpret("doc/examples/pcf.sum.spl");
-//        interpret("doc/examples/pcf.minus.spl");
-//        interpret("doc/examples/pcf.even.spl");
-//        interpret("doc/examples/pcf.fibonacci.spl");
-//        interpret("doc/examples/pcf.factorial.spl");
-//        interpret("doc/examples/gcd1.spl");
-//        interpret("doc/examples/factorial.spl");
-//        interpret("doc/examples/gcd2.spl");
-//        interpret("doc/examples/sum.spl");
-//        interpret("doc/examples/map.spl");
-//        interpret("doc/examples/pcf.lists.spl");
+        interpret("doc/examples/plus.spl");
+        interpret("doc/examples/max.spl");
+        interpret("doc/examples/pcf.sum.spl");
+        interpret("doc/examples/pcf.minus.spl");
+        interpret("doc/examples/pcf.even.spl");
+        interpret("doc/examples/pcf.fibonacci.spl");
+        interpret("doc/examples/pcf.factorial.spl");
+        interpret("doc/examples/gcd1.spl");
+        interpret("doc/examples/factorial.spl");
+        interpret("doc/examples/gcd2.spl");
+        interpret("doc/examples/sum.spl");
+        interpret("doc/examples/map.spl");
+        interpret("doc/examples/pcf.lists.spl");
 //        //Bad cases
-//        interpret("doc/examples/pcf.twice.spl");
+//        interpret("doc/examples/pcf.twice.spl");//可以求值但类型报错
 //        interpret("doc/examples/true.spl");
     }
 }
